3060e1d3f77acde7a1daa0acdf6e87e557ef1ba6
Joe Woods <code@joewoods.dev>
2022-01-31 21:43:42 -0500

    Format code

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import TurndownService from 'turndown';
+import TurndownService from "turndown";
 
 const term = process.argv[2];
 
@@ -16,22 +16,26 @@ const fetchAndMatchTerms = async function () {
   const result = await fetch(`https://hnrss.org/newcomments?q=${term}`);
   const body = await result.text();
   const items = body.matchAll(/<item>[\s\S]*?<\/item>/gm);
-  Array.from(items).reverse().forEach((item) => {
-    const description = item[0].match(/<description>([\s\S]*)<\/description>/m);
-    const comment = description && description[1];
-    const link = item[0].match(/<link>([\s\S]*)<\/link>/m);
-    if (!seen[link]) {
-      const date = item[0].match(/<pubDate>([\s\S]*)<\/pubDate>/m);
-      console.log();
-      console.log("=============");
-      console.log();
-      console.log(link[1]);
-      console.log(date[1]);
-      console.log();
-      console.log(turndownService.turndown(comment.slice(10, -4)));
-      seen[link] = true;
-    }
-  });
+  Array.from(items)
+    .reverse()
+    .forEach((item) => {
+      const description = item[0].match(
+        /<description>([\s\S]*)<\/description>/m
+      );
+      const comment = description && description[1];
+      const link = item[0].match(/<link>([\s\S]*)<\/link>/m);
+      if (!seen[link]) {
+        const date = item[0].match(/<pubDate>([\s\S]*)<\/pubDate>/m);
+        console.log();
+        console.log("=============");
+        console.log();
+        console.log(link[1]);
+        console.log(date[1]);
+        console.log();
+        console.log(turndownService.turndown(comment.slice(10, -4)));
+        seen[link] = true;
+      }
+    });
 
   setTimeout(fetchAndMatchTerms, 2000);
 };
